005545a20c8a98ace659e35c8e18558388c2a134
KimDONGCHAN <kdch6690@gmail.com>
2025-02-04 00:37:56 +0900

    n+1 카드게임 정답

--- n+1카드게임.py
+++ n+1카드게임.py
@@ -5,6 +5,8 @@
 2. 1에서 불가능하다면, 추가적으로 뽑은 카드 중 1개를 사용하여 N+1을 만든다.
 3. 2에서 불가능하다면, 추가적으로 뽑은 카드 2개를 이용하여 N+1을 만든다.
 4. 3에서 불가능하다면, 다음 라운드로의 진행이 불가능하다.
+
+링크: https://tech.kakao.com/posts/610
 """
 
 from collections import deque
@@ -14,55 +16,76 @@ def solution(coin, cards):
     return answer
 
 def get2Card(tmp, card_batch):
+
     for _ in range(2):
         if  card_batch:
             tmp.add(card_batch.popleft())
+    
+    
+def checkEmpty(arr):
+    if arr:
+       return False
+    else:
+       return True
 
 def simulate(coin, cards):
-    round_num = 1
+    round_num = 0
     n = max(cards)
     handle = set(cards[0:n//3])
     card_batch = deque(cards[n//3:])
     tmp = set()
-
-
-    get2Card(tmp, card_batch)
+    
     while True:
         is_be = False
+        round_num += 1
+        is_end = checkEmpty(card_batch)
+        
+        get2Card(tmp, card_batch)
 
-        for x in list(handle):
-            if n+1-x in handle:
-                
-                handle.remove(x)
-                handle.remove(n+1-x)
-                round_num += 1
-                get2Card(tmp, card_batch)
-                is_be = True
-            
-        for x in list(handle): #1개만 있는 경우
-            if n+1-x in tmp:
-                handle.remove(x)
-                tmp.remove(n+1-x)
-                round_num += 1
-                coin -= 1
-                get2Card(tmp, card_batch)
-                is_be = True
-
-        for x in list(tmp):
-
-            if n+1-x in tmp and coin >= 2:
-                
-                tmp.remove(x)
-                tmp.remove(n+1-x)
-                round_num += 1
-                coin -= 2
-                get2Card(tmp, card_batch)
-                is_be = True     
-
-        if not card_batch or not is_be:
+        if is_end:
             break
 
+        for x in list(handle):
+            if n+1 - x in handle:
+                is_be = True
 
+                handle.remove(x)
+                handle.remove(n+1-x)
+                break
+
+
+        if is_be:
+            continue
+
+        for x in list(handle):
+            if n+1 -x in tmp and coin >= 1:
+                is_be = True
+
+                handle.remove(x)
+                tmp.remove(n+1-x)
+                coin -= 1
+                break
+
+        if is_be:
+            continue
+
+        for x in list(tmp):
+            if n+1 -x in tmp and coin >= 2:
+                is_be = True
+
+                tmp.remove(x)
+                tmp.remove(n+1-x)
+                coin -= 2
+                break
+
+        if is_be:
+            continue
+        else:
+            break
+        
+    
     return round_num
 
-print(solution(10,	[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]))
+
+
+print(solution(2, [5, 8, 1, 2, 9, 4, 12, 11, 3, 10, 6, 7]))
